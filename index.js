f3e103a3a7465b98e9f2c8718d568728745d6a65
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-11-05 19:48:42 +0530

    Carousal image changes

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ $('.carousel .carousel-item').each(function () {
     next.children(':first-child').clone().appendTo($(this));
     
     for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
+
+// Get modal and overlay elements
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+const modalImage = document.getElementById("modalImage");
+
+// Function to open the modal
+function openModal(slideNumber) {
+    modalImage.src = `/carousel-image/image${slideNumber}.jpg`;
+    modal.style.display = "block";
+    overlay.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = "none";
+    overlay.style.display = "none";
+    modalImage.src = "";
+}
